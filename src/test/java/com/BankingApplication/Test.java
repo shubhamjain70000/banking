7fb239fa45418bb4d02792e72f7881acf3a8fb40
Shubham <shubhamjain02081996@gmail.com>
2023-03-12 12:09:32 +0530

    new changes4

--- src/test/java/com/BankingApplication/Test.java
+++ src/test/java/com/BankingApplication/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		System.out.println("Test4");
 	}
 
 }
